6bf96deada5e9d1daad89f60c48dc29fe4518985
elsaneth <elisabeth.suits@gmail.com>
2023-11-23 20:23:37 +0200

    jump_search(ex3) pseudo + python and descrption

---
+++ ex3/jump_search.py
@@ -0,0 +1,27 @@
+import math
+
+def jump_search(arr, target):
+    length = len(arr)
+    step = int(math.sqrt(length))
+    prev = 0
+    
+    while arr[int(min(step, length)-1)] < target:
+        prev = step
+        step += int(math.sqrt(length))
+        if prev >= length:
+            result = "No such nr in array"
+            return result
+    
+    for i in range (prev, min(step, length)):
+        if arr[i] == target:
+            result = f"This nr exists in array on index {i}"
+            return result
+    result = "No such nr in array"
+    return result
+
+# Driver code to test function
+arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+target = 55
+
+# Find the indetarget of 'target' using Jump Search
+print(jump_search(arr, target))
